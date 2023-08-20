3f5d7beee36e67d16fa7099ff83e9df10708b252
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-08-20 15:56:57 +0200

    1

--- js/Script.js
+++ js/Script.js
@@ -1,56 +1,137 @@
-$(document).ready(function () {
-    const tripItems = [
-      {
-        id: "trip1",
-        title: "Paris Getaway",
-        description: "Explore the beauty of the City of Love, Paris!",
-      },
-      {
-        id: "trip2",
-        title: "New York Adventure",
-        description: "Experience the excitement of the Big Apple, New York City!",
-      },
-      {
-        id: "trip3",
-        title: "Tropical Paradise",
-        description: "Relax and unwind on a tropical paradise island!",
-      },
-      {
-        id: "trip4",
-        title: "Mountain Retreat",
-        description: "Escape to the serene beauty of the mountains!",
-      },
-      {
-        id: "trip5",
-        title: "Cultural Exploration",
-        description: "Immerse yourself in diverse cultures and traditions!",
-      },
-    ];
-  
-    function createTripItemCard(item) {
-      const card = $("<div>").addClass("trip-card");
-      card.html(`
-        <h3 id="trip-title">${item.title}</h3>
-        <p id="trip-description">${item.description}</p>
-        <button id="purchase-ticket">Purchase Ticket</button>
-      `);
-      return card;
-    }
-  
-    function displayTripDetails(tripId) {
-      const tripDetails = $("#trip-details");
-      tripDetails.empty();
-  
-      const selectedTrip = tripItems.find(item => item.id === tripId);
-      if (selectedTrip) {
-        const tripCard = createTripItemCard(selectedTrip);
-        tripDetails.append(tripCard);
-      }
-    }
-  
-    $("#trip-options li").on("click", function () {
-      const tripId = $(this).attr("data-trip-id");
-      displayTripDetails(tripId);
+// Change h1 text after document load
+document.addEventListener("DOMContentLoaded", function() {
+  const cruiseNameElement = document.getElementById("cruiseName");
+  cruiseNameElement.textContent = "Welcome to Your Cruise Name";
+});
+
+// Fetch weather data from Open Weather API
+fetchWeatherData();
+
+function fetchWeatherData() {
+  const apiKey = "YOUR_OPEN_WEATHER_API_KEY";
+  const city = "YOUR_CITY_NAME";
+  const weatherDataElement = document.getElementById("weatherData");
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const weatherDescription = data.weather[0].description;
+      const temperature = (data.main.temp - 273.15).toFixed(2); // Convert to Celsius
+
+      weatherDataElement.innerHTML = `<p>Weather: ${weatherDescription}</p><p>Temperature: ${temperature}°C</p>`;
+    })
+    .catch(error => {
+      console.error("Error fetching weather data:", error);
     });
+}
+
+// Create accordion sections
+const accordionSections = [
+  { title: "Cruise Map", content: "Content for Cruise Map section." },
+  { title: "Additional Information", content: "Content for Additional Information section." },
+  { title: "FAQ", content: "Content for FAQ section." }
+];
+
+const accordionSectionsElement = document.getElementById("accordionSections");
+accordionSections.forEach((section, index) => {
+  const sectionId = `section${index}`;
+  accordionSectionsElement.innerHTML += `
+    <div class="card">
+      <div class="card-header" id="${sectionId}">
+        <h2 class="mb-0">
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${sectionId}Collapse" aria-expanded="true" aria-controls="${sectionId}Collapse">
+            ${section.title}
+          </button>
+        </h2>
+      </div>
+
+      <div id="${sectionId}Collapse" class="collapse" aria-labelledby="${sectionId}" data-parent="#accordionSections">
+        <div class="card-body">
+          ${section.content}
+        </div>
+      </div>
+    </div>
+  `;
+});
+
+
+// Create trip options
+const tripOptions = [
+  { name: "Trip 1", duration: 3, destinations: ["Destination A"], price: 1500 },
+  { name: "Trip 2", duration: 7, destinations: ["Destination A", "Destination B"], price: 2500 },
+  // ... (more trip options)
+];
+
+const tripOptionsElement = document.getElementById("tripOptions");
+tripOptions.forEach((trip, index) => {
+  const tripCard = document.createElement("div");
+  tripCard.classList.add("tripCard");
+  tripCard.innerHTML = `
+    <h3>${trip.name}</h3>
+    <p>Duration: ${trip.duration} days</p>
+    <p>Destinations: ${trip.destinations.join(", ")}</p>
+    <p>Price: ${trip.price} ZAR</p>
+    <button class="purchaseBtn" style="display: none;">Purchase Ticket</button>
+  `;
+
+  tripCard.addEventListener("click", () => {
+    // Clear previous active trip
+    const activeTrip = document.querySelector(".activeTrip");
+    if (activeTrip) {
+      activeTrip.classList.remove("activeTrip");
+      activeTrip.querySelector(".purchaseBtn").style.display = "none";
+    }
+
+    // Set new active trip
+    tripCard.classList.add("activeTrip");
+    tripCard.querySelector(".purchaseBtn").style.display = "block";
   });
-  
+
+  tripOptionsElement.appendChild(tripCard);
+});
+
+
+// Create purchases table
+const purchasesTableElement = document.getElementById("purchasesTable");
+const removeAllBtn = document.getElementById("removeAllBtn");
+
+removeAllBtn.addEventListener("click", () => {
+  removeAllPurchases();
+});
+
+function removeAllPurchases() {
+  const purchasesRows = purchasesTableElement.querySelectorAll("tbody tr");
+  purchasesRows.forEach(row => {
+    row.remove();
+  });
+}
+
+// Add purchases to the table
+function addPurchaseToTable(tripCode, ticketQuantity, totalCost) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${tripCode}</td>
+    <td>${ticketQuantity}</td>
+    <td>${totalCost} ZAR</td>
+    <td><button class="removePurchaseBtn">Remove</button></td>
+  `;
+
+  const removePurchaseBtn = row.querySelector(".removePurchaseBtn");
+  removePurchaseBtn.addEventListener("click", () => {
+    row.remove();
+  });
+
+  purchasesTableElement.querySelector("tbody").appendChild(row);
+}
+
+// "Purchase" button functionality
+const purchaseButton = document.createElement("button");
+purchaseButton.innerText = "Purchase";
+purchaseButton.addEventListener("click", () => {
+  showSuccessMessage();
+});
+
+function showSuccessMessage() {
+  alert("Successful Purchase");
+}
+
